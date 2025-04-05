2e74c7d664561090e7f69b0588c9f4a5a9398346
Dmitry <Moskalev@yandex.ru>
2025-04-05 10:51:13 +0300

    Допилил арифметические действия
    Калькулятор полностью работает, осталось проверки допилить

--- Calc.py
+++ Calc.py
@@ -2,13 +2,21 @@ def summary_numbs(first,sign,second):
     res = 0
     if sign == "+":
         res = first + second
+    if sign == "-":
+        res = first - second
+    if sign == "*":
+        res = first * second
+    if sign == "/":
+        res = first / second
+    if sign == "^":
+        res = first ** second
     return res
 
 
 def main():
 
     first = int(input(f"Введите первое число: "))
-    sign = input(f"Введите знак: ")
+    sign = input(f"Введите знак '+','-','*','/','^': ")
     second = int(input(f"Введите второе число: "))
     res = (summary_numbs(first,sign,second))
     print(f"{first} {sign} {second} = {res}")
