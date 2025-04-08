e70817b9cd9dd43d9024359cfe95abcf067f954c
Dmitry <Moskalev@yandex.ru>
2025-04-08 20:39:58 +0300

    Реорганизация файловой системы

---
+++ source/classwork/Calc.py
@@ -0,0 +1,35 @@
+def summary_numbs(first,sign,second):
+    """
+    Функция кальклятор
+    :param first:
+    :param sign:
+    :param second:
+    :return:
+    """
+    res = 0
+    if sign == "+":
+        res = first + second
+    if sign == "-":
+        res = first - second
+    if sign == "*":
+        res = first * second
+    if sign == "/":
+        res = first / second
+    if sign == "^":
+        res = first ** second
+    return res
+
+
+def main():
+
+    first = int(input(f"Введите первое число: "))
+    sign = input(f"Введите знак '+','-','*','/','^': ")
+    second = int(input(f"Введите второе число: "))
+    res = (summary_numbs(first,sign,second))
+    print(f"{first} {sign} {second} = {res}")
+
+
+
+
+if __name__ == "__main__":
+    main()
