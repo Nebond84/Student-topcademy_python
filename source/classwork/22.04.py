99e22fc370134ec7f6f062a00feedd84da08263a
Dmitry <moskalyevi76@gmail.com>
2025-04-23 11:24:06 +0300

    Работа в классе 22.04

---
+++ source/classwork/22.04.py
@@ -0,0 +1,17 @@
+import random
+
+# square = lambda numer : numer * numer
+# print(square(2))
+#
+# def Equalse(first: int,func):
+#     return f"{first} ** 2 = {func(first)}"
+#
+# print(Equalse(4,lambda nummer: nummer * nummer))
+
+#Сортировки
+
+def Bublesort(lst):
+    lst_2 = list()
+    for i in range(0,100000):
+        lst_2.append(random.randint(-32567,32567))
+print(Bublesort(lst_2))
