0bcff626189e1d3172869a4e37f0b0a1c6050c36
Dmitry <moskalyevi76@gmail.com>
2025-04-30 14:14:38 +0300

    Работа в классе
    Упражнение работы с файлами

---
+++ source/classwork/function/function.py
@@ -0,0 +1,50 @@
+def functionPrint(lst : list):
+    for elem in lst:
+        print(f"Hello, {elem}", end="! | ")
+    print()
+
+def functionPrintRevers(lst : list):
+    for elem in lst:
+        print(f"{elem}, hello", end="! | ")
+    print()
+
+def getPrintFunction(reversFlag : bool):
+    if reversFlag:
+        return functionPrintRevers
+    else:
+        return functionPrint
+
+def summer(First : int, Second: int) -> int:
+    return First + Second
+
+def minus(First : int, Second: int) -> int:
+    return First - Second
+
+def multiply(First : int, Second: int) -> int:
+    return First * Second
+
+def division(First : int, Second: int) -> int:
+    if (not Second):
+        return 0
+    else:
+        return First + Second
+
+def steps(First : int, Second: int) -> int:
+    return First ** Second
+
+def stepsFromPlus(First : int , Second: int) -> int:
+    result = First
+    for i in range(1, Second):
+        tmp = result
+        for j in range(1, First + 1):
+           tmp += result
+        result = tmp
+    return result
+
+def getMathFunc(sign : str):
+    match sign:
+        case "+": return summer
+        case "-": return minus
+        case "*": return multiply
+        case "/": return division
+        case "^": return steps
--- source/classwork/main.py
+++ source/classwork/main.py
@@ -1,1 +1,42 @@
-print(f"Hello world")
+from function.function import getMathFunc as mFunc
+import os
+
+def getResult(example : str) -> int:
+    First = ""
+    Second = ""
+    Sign = ""
+    temp = ""
+    for char in example:
+        if char in "+-*/^":
+            First = int(temp)
+            Sign = char
+            temp = ""
+        elif char in "\n":
+            continue
+        else:
+            temp += char
+    Second = int(temp)
+    func = mFunc(Sign)
+    return func(First, Second)
+
+def getFile():
+    resultLst = list()
+    # file = open("resources/name.txt", "r+")
+    with open("name.txt", "r+") as file:
+        for line in file:
+            line = line.removesuffix("\n")
+            result = getResult(line)
+            resultLst.append(f"{line} = {result}\n")
+
+    return resultLst
+
+os.chdir("resources")
+results = getFile()
+if not os.path.isdir("result"):
+    os.mkdir("result")
+
+os.chdir("result")
+# os.kill(9288,9)
+with open("results.txt", "w") as file:
+    for line in results:
+        file.write(line)
